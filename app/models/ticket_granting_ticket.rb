b87b800640680660669efa7465cedf8a860c1624
Nils Caspar <ncaspar@me.com>
2012-11-22 20:11:52 +0100

    serialize extra_attributes (Hash)

--- app/models/ticket_granting_ticket.rb
+++ app/models/ticket_granting_ticket.rb
@@ -1,4 +1,5 @@
 class TicketGrantingTicket < ActiveRecord::Base
-  attr_accessible :ticket, :username, :user_agent
+  attr_accessible :ticket, :username, :user_agent, :extra_attributes
+  serialize :extra_attributes, Hash
   validates :ticket, uniqueness: true
 end
