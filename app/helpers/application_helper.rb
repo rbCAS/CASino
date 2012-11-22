1df0cdd47d13331dcbef4f1557d86a9a3d39f550
Nils Caspar <ncaspar@me.com>
2012-11-22 20:11:09 +0100

    include timestamp in ticket-identifier so it is unique

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def random_ticket_string(prefix, length = 40)
+    random_string = rand(36**length).to_s(36)
+    "#{prefix}-#{Time.now.to_i}-#{random_string}"
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,6 @@
 module SessionsHelper
   def acquire_login_ticket
-    ticket = false
-    while !ticket
-      ticket = LoginTicket.create ticket: 'LT-' + SecureRandom.urlsafe_base64(30)
-    end
+    ticket = LoginTicket.create ticket: random_ticket_string('LT')
     logger.debug "Created login ticket '#{ticket.ticket}'"
     ticket
   end
