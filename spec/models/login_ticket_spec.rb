426f1b739a11a37f62018909b249ba7a2d012359
Nils Caspar <ncaspar@me.com>
2012-11-24 11:47:52 +0100

    show which method we are testing

--- spec/models/login_ticket_spec.rb
+++ spec/models/login_ticket_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe LoginTicket do
-  it 'should delete old tickets' do
-    ticket = LoginTicket.new ticket: 'LT-12345'
-    ticket.save!
-    ticket.created_at = 10.hours.ago
-    ticket.save!
-    lambda do
-      LoginTicket.cleanup
-    end.should change(LoginTicket, :count).by(-1)
-    LoginTicket.find_by_ticket('LT-12345').should be_false
+  describe '.cleanup' do
+    it 'should delete old tickets' do
+      ticket = LoginTicket.new ticket: 'LT-12345'
+      ticket.save!
+      ticket.created_at = 10.hours.ago
+      ticket.save!
+      lambda do
+        LoginTicket.cleanup
+      end.should change(LoginTicket, :count).by(-1)
+      LoginTicket.find_by_ticket('LT-12345').should be_false
+    end
   end
 end
