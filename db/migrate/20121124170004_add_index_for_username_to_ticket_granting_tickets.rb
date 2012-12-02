a6fe50a721cf1d0aee52731328e2c7b63e4d65a8
Nils Caspar <ncaspar@me.com>
2012-12-02 20:04:46 +0100

    extract schema

--- db/migrate/20121124170004_add_index_for_username_to_ticket_granting_tickets.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexForUsernameToTicketGrantingTickets < ActiveRecord::Migration
-  def change
-    add_index :ticket_granting_tickets, :username
-  end
-end
--- db/migrate/20121124183732_add_ticket_indexes.rb
+++
@@ -1,6 +0,0 @@
-class AddTicketIndexes < ActiveRecord::Migration
-  def change
-    add_index :ticket_granting_tickets, :ticket
-    add_index :login_tickets, :ticket
-  end
-end
