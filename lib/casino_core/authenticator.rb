e84a4b160b6faead479024e43cea7acdf53a88d1
Nils Caspar <ncaspar@me.com>
2012-12-02 19:31:25 +0100

    fixed methods

--- lib/casino_core/authenticator.rb
+++ lib/casino_core/authenticator.rb
@@ -1,13 +1,6 @@
-class CASinoCore::Authenticator
-  autoload :Static, 'casino_core/authenticator/static.rb'
-
-  def self.included(authenticator)
-    authenticator.extend(ClassMethods)
-  end
-
-  class ClassMethods
-    def initialize(options)
-    end
+module CASinoCore
+  class Authenticator
+    autoload :Static, 'casino_core/authenticator/static.rb'
 
     def validate(username, password)
       raise NotImplementedError, "This method must be implemented by a class extending #{self.class}"
