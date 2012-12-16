c5b6e3997cec4a0e53a2a1c66b7b994be081c90f
Nils Caspar <ncaspar@me.com>
2012-12-16 16:00:35 +0100

    extracted models

--- app/models/proxy_granting_ticket.rb
+++
@@ -1,6 +0,0 @@
-class ProxyGrantingTicket < ActiveRecord::Base
-  attr_accessible :iou, :ticket, :ticket_granting_ticket_id
-  validates :ticket, uniqueness: true
-  validates :iou, uniqueness: true
-  belongs_to :ticket_granting_ticket
-end
