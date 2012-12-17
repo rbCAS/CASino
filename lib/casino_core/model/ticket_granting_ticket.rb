903d0563a02b36abcaaa99b75ebbdb7e85077318
Nils Caspar <ncaspar@me.com>
2012-12-17 14:18:09 +0100

    Added method to extract some human-readable browser info

--- lib/casino_core/model/ticket_granting_ticket.rb
+++ lib/casino_core/model/ticket_granting_ticket.rb
@@ -6,4 +6,9 @@ class CASinoCore::Model::TicketGrantingTicket < ActiveRecord::Base
   validates :ticket, uniqueness: true
   has_many :service_tickets
   has_many :proxy_granting_tickets
+
+  def browser_info
+    user_agent = UserAgent.parse(self.user_agent)
+    "#{user_agent.browser} (#{user_agent.platform})"
+  end
 end
