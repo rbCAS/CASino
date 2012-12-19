6c793749de22bfd6b400843f787cd1ff2132a970
Nils Caspar <ncaspar@me.com>
2012-12-19 20:52:56 +0100

    Logger is a setting now

--- lib/casino_core/helper/logger.rb
+++ lib/casino_core/helper/logger.rb
@@ -1,11 +1,10 @@
+require 'casino_core/settings'
+
 module CASinoCore
   module Helper
     module Logger
       def logger
-        # TODO this is just a "silent logger", make logger a setting!
-        logger = ::Logger.new(STDOUT)
-        logger.level = ::Logger::Severity::UNKNOWN
-        logger
+        CASinoCore::Settings.logger
       end
     end
   end
--- lib/casino_core/settings.rb
+++ lib/casino_core/settings.rb
@@ -3,13 +3,17 @@ require 'casino_core/authenticator'
 module CASinoCore
   class Settings
     class << self
-      attr_accessor :login_ticket, :service_ticket, :authenticators
+      attr_accessor :login_ticket, :service_ticket, :authenticators, :logger
       def init(config = {})
         config.each do |key,value|
           if respond_to?("#{key}=")
             send("#{key}=", value)
           end
         end
+      end
+
+      def logger
+        @logger || ::Logger.new(STDOUT)
       end
 
       def authenticators=(authenticators)
