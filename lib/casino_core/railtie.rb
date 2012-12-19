e94008f7b623d7695a0a7ac781906b9b36bd67fc
Nils Caspar <ncaspar@me.com>
2012-12-19 21:00:31 +0100

    Use Rails.logger as logger

--- lib/casino_core/railtie.rb
+++ lib/casino_core/railtie.rb
@@ -7,8 +7,11 @@ module CASinoCore
       CASinoCore::RakeTasks.load_tasks
     end
 
-    initializer "casino_core.load_configuration" do
+    initializer 'casino_core.load_configuration' do
       CASinoCore.setup Rails.env
     end
+
+    initializer 'casino_core.setup_logger' do
+      CASinoCore::Settings.logger = Rails.logger
   end
 end
