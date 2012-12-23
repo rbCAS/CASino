2a65aff4c37956a77a8a25cf441aa1d75ccd9018
Nils Caspar <ncaspar@me.com>
2012-12-23 14:18:26 +0100

    Proxy-granting tickets belong to service tickets

--- lib/casino_core/model/proxy_granting_ticket.rb
+++ lib/casino_core/model/proxy_granting_ticket.rb
@@ -1,8 +1,8 @@
 require 'casino_core/model'
 
 class CASinoCore::Model::ProxyGrantingTicket < ActiveRecord::Base
-  attr_accessible :iou, :ticket, :ticket_granting_ticket_id
+  attr_accessible :iou, :ticket, :service_ticket_id
   validates :ticket, uniqueness: true
   validates :iou, uniqueness: true
-  belongs_to :ticket_granting_ticket
+  belongs_to :service_ticket
 end
