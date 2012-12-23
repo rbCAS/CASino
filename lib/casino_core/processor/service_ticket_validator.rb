f0aa0eb41b2fc00a7ab27dea067df263e2ba3d23
Nils Caspar <ncaspar@me.com>
2012-12-23 13:17:31 +0100

    Include extra attributes

--- lib/casino_core/processor/service_ticket_validator.rb
+++ lib/casino_core/processor/service_ticket_validator.rb
@@ -36,6 +36,13 @@ class CASinoCore::Processor::ServiceTicketValidator < CASinoCore::Processor
         ticket_granting_ticket = options[:service_ticket].ticket_granting_ticket
         service_response.cas :authenticationSuccess do |authentication_success|
           authentication_success.cas :user, ticket_granting_ticket.username
+          unless ticket_granting_ticket.extra_attributes.blank?
+            authentication_success.cas :attributes do |attributes|
+              ticket_granting_ticket.extra_attributes.each do |key, value|
+                serialize_extra_attribute(attributes, key, value)
+              end
+            end
+          end
           if options[:proxy_granting_ticket]
             proxy_granting_ticket = options[:proxy_granting_ticket]
             authentication_success.cas :proxyGrantingTicket, proxy_granting_ticket.iou
@@ -46,5 +53,17 @@ class CASinoCore::Processor::ServiceTicketValidator < CASinoCore::Processor
       end
     end
     xml.target!
+  end
+
+  def serialize_extra_attribute(builder, key, value)
+    if value.kind_of?(String) || value.kind_of?(Numeric) || value.kind_of?(Symbol)
+      builder.cas key, "#{value}"
+    elsif value.kind_of?(Numeric)
+      builder.cas key, value.to_s
+    else
+      builder.cas key do
+        builder.cdata! value.to_yaml
+      end
+    end
   end
 end
