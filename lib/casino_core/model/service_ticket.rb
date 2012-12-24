3b663940ee044425764f1c4fd76d5a452cd98411
Nils Caspar <ncaspar@me.com>
2012-12-24 11:52:03 +0100

    Always use #destroy but only send SSOut notification for consumed

--- lib/casino_core/model/service_ticket.rb
+++ lib/casino_core/model/service_ticket.rb
@@ -8,11 +8,11 @@ class CASinoCore::Model::ServiceTicket < ActiveRecord::Base
   attr_accessible :ticket, :service
   validates :ticket, uniqueness: true
   belongs_to :ticket_granting_ticket
-  before_destroy :send_single_sing_out_notification
+  before_destroy :send_single_sing_out_notification, if: :consumed?
   has_many :proxy_granting_tickets
 
   def self.cleanup_unconsumed
-    self.delete_all(['created_at < ? AND consumed = ?', CASinoCore::Settings.service_ticket[:lifetime_unconsumed].seconds.ago, false])
+    self.destroy_all(['created_at < ? AND consumed = ?', CASinoCore::Settings.service_ticket[:lifetime_unconsumed].seconds.ago, false])
   end
 
   def self.cleanup_consumed
