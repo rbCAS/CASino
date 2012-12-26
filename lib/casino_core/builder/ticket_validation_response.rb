17e7898b14baaee9e254da045de2bb6d4012e844
Nils Caspar <ncaspar@me.com>
2012-12-26 18:59:05 +0100

    Refactoring

--- lib/casino_core/builder/ticket_validation_response.rb
+++ lib/casino_core/builder/ticket_validation_response.rb
@@ -24,29 +24,10 @@ class CASinoCore::Builder::TicketValidationResponse < CASinoCore::Builder
         else
           ticket_granting_ticket = ticket.ticket_granting_ticket
         end
-        service_response.cas :authenticationSuccess do |authentication_success|
-          authentication_success.cas :user, ticket_granting_ticket.username
-          unless ticket_granting_ticket.extra_attributes.blank?
-            authentication_success.cas :attributes do |attributes|
-              ticket_granting_ticket.extra_attributes.each do |key, value|
-                serialize_extra_attribute(attributes, key, value)
-              end
-            end
-          end
-          if @options[:proxy_granting_ticket]
-            proxy_granting_ticket = @options[:proxy_granting_ticket]
-            authentication_success.cas :proxyGrantingTicket, proxy_granting_ticket.iou
-          end
-          if ticket.is_a?(CASinoCore::Model::ProxyTicket)
-            authentication_success.cas :proxies do |proxies_container|
-              proxies.each do |proxy|
-                proxies_container.cas :proxy, proxy
-              end
-            end
-          end
-        end
+
+        build_success_xml(service_response, ticket, ticket_granting_ticket, proxies)
       else
-        service_response.cas :authenticationFailure, @options[:error_message], code: @options[:error_code]
+        build_failure_xml(service_response)
       end
     end
     xml.target!
@@ -63,5 +44,33 @@ class CASinoCore::Builder::TicketValidationResponse < CASinoCore::Builder
         container.cdata! value.to_yaml
       end
     end
+  end
+
+  def build_success_xml(service_response, ticket, ticket_granting_ticket, proxies)
+    service_response.cas :authenticationSuccess do |authentication_success|
+      authentication_success.cas :user, ticket_granting_ticket.username
+      unless ticket_granting_ticket.extra_attributes.blank?
+        authentication_success.cas :attributes do |attributes|
+          ticket_granting_ticket.extra_attributes.each do |key, value|
+            serialize_extra_attribute(attributes, key, value)
+          end
+        end
+      end
+      if @options[:proxy_granting_ticket]
+        proxy_granting_ticket = @options[:proxy_granting_ticket]
+        authentication_success.cas :proxyGrantingTicket, proxy_granting_ticket.iou
+      end
+      if ticket.is_a?(CASinoCore::Model::ProxyTicket)
+        authentication_success.cas :proxies do |proxies_container|
+          proxies.each do |proxy|
+            proxies_container.cas :proxy, proxy
+          end
+        end
+      end
+    end
+  end
+
+  def build_failure_xml(service_response)
+    service_response.cas :authenticationFailure, @options[:error_message], code: @options[:error_code]
   end
 end
