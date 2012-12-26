66419a8627da03b75a1f78d5447902cc63187b39
Nils Caspar <ncaspar@me.com>
2012-12-26 21:02:04 +0100

    Destroy dependents

--- lib/casino_core/model/proxy_granting_ticket.rb
+++ lib/casino_core/model/proxy_granting_ticket.rb
@@ -5,5 +5,5 @@ class CASinoCore::Model::ProxyGrantingTicket < ActiveRecord::Base
   validates :ticket, uniqueness: true
   validates :iou, uniqueness: true
   belongs_to :granter, polymorphic: true
-  has_many :proxy_tickets
+  has_many :proxy_tickets, dependent: :destroy
 end
--- lib/casino_core/model/proxy_ticket.rb
+++ lib/casino_core/model/proxy_ticket.rb
@@ -6,7 +6,7 @@ class CASinoCore::Model::ProxyTicket < ActiveRecord::Base
   attr_accessible :ticket, :service
   validates :ticket, uniqueness: true
   belongs_to :proxy_granting_ticket
-  has_many :proxy_granting_tickets, as: :granter
+  has_many :proxy_granting_tickets, as: :granter, dependent: :destroy
 
   def self.cleanup_unconsumed
     self.destroy_all(['created_at < ? AND consumed = ?', CASinoCore::Settings.proxy_ticket[:lifetime_unconsumed].seconds.ago, false])
