a1f019133557859f61d3228b04a15df2bc07e36c
Raffael Schmid <raffael@yux.ch>
2012-12-30 17:32:10 +0100

    use the new helper methods also for normal logins

--- lib/casino_core/processor/login_credential_acceptor.rb
+++ lib/casino_core/processor/login_credential_acceptor.rb
@@ -6,6 +6,8 @@ class CASinoCore::Processor::LoginCredentialAcceptor < CASinoCore::Processor
   include CASinoCore::Helper::Logger
   include CASinoCore::Helper::LoginTickets
   include CASinoCore::Helper::ServiceTickets
+  include CASinoCore::Helper::Authentication
+  include CASinoCore::Helper::TicketGrantingTickets
 
   # Use this method to process the request. It expects the username in the parameter "username" and the password
   # in "password".
@@ -54,27 +56,4 @@ class CASinoCore::Processor::LoginCredentialAcceptor < CASinoCore::Processor
     end
   end
 
-  def validate_login_credentials(username, password)
-    authentication_result = nil
-    CASinoCore::Settings.authenticators.each do |authenticator_name, authenticator|
-      data = authenticator.validate(username, password)
-      if data
-        authentication_result = { authenticator: authenticator_name, user_data: data }
-        logger.info("Credentials for username '#{data[:username]}' successfully validated using authenticator '#{authenticator_name}' (#{authenticator.class})")
-        break
-      end
-    end
-    authentication_result
-  end
-
-  def acquire_ticket_granting_ticket(authentication_result, user_agent = nil)
-    user_data = authentication_result[:user_data]
-    CASinoCore::Model::TicketGrantingTicket.create!({
-      ticket: random_ticket_string('TGC'),
-      authenticator: authentication_result[:authenticator],
-      username: user_data[:username],
-      extra_attributes: user_data[:extra_attributes],
-      user_agent: user_agent
-    })
-  end
 end
