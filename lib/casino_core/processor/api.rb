af84a07edae4c2c53f62c556e2d5f71b8fbcd4ce
Raffael Schmid <raffael@yux.ch>
2012-12-30 20:56:44 +0100

    add an api logout processor and tests

--- lib/casino_core/processor/api.rb
+++ lib/casino_core/processor/api.rb
@@ -3,6 +3,7 @@ module CASinoCore
     module API
       autoload :LoginCredentialAcceptor, 'casino_core/processor/api/login_credential_acceptor.rb'
       autoload :ServiceTicketProvider, 'casino_core/processor/api/service_ticket_provider.rb'
+      autoload :Logout, 'casino_core/processor/api/logout.rb'
     end
   end
 end
