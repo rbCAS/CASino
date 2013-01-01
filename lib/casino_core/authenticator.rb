fd2f9e140d4d400d79dfef7423ee2fa4cce543c3
Nils Caspar <ncaspar@me.com>
2013-01-01 13:31:08 +0100

    Error for Authenticators

--- lib/casino_core/authenticator.rb
+++ lib/casino_core/authenticator.rb
@@ -2,6 +2,8 @@ module CASinoCore
   class Authenticator
     autoload :Static, 'casino_core/authenticator/static.rb'
 
+    class AuthenticatorError < StandardError; end
+
     def validate(username, password)
       raise NotImplementedError, "This method must be implemented by a class extending #{self.class}"
     end
