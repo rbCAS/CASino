bd3f6a2422a69ec4526915dc66c5dcc5b051940c
Nils Caspar <ncaspar@me.com>
2013-02-08 22:53:19 +0100

    Add support for two-factor authentication

--- spec/controllers/listener/second_factor_authentication_acceptor_spec.rb
+++ spec/controllers/listener/second_factor_authentication_acceptor_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-describe CASino::Listener::LoginCredentialAcceptor do
+describe CASino::Listener::SecondFactorAuthenticationAcceptor do
   include CASino::Engine.routes.url_helpers
   let(:controller) { Struct.new(:cookies).new(cookies: {}) }
   let(:listener) { described_class.new(controller) }
 
   before(:each) do
     controller.stub(:redirect_to)
+  end
+
+  describe '#user_not_logged_in' do
+    it 'redirects to the login page' do
+      controller.should_receive(:redirect_to).with(login_path)
+      listener.user_not_logged_in
+    end
   end
 
   describe '#user_logged_in' do
@@ -33,30 +40,17 @@ describe CASino::Listener::LoginCredentialAcceptor do
     end
   end
 
-  [:invalid_login_credentials, :invalid_login_ticket].each do |method|
-    context "##{method}" do
-      let(:login_ticket) { Object.new }
-      let(:flash) { ActionDispatch::Flash::FlashHash.new }
+  context "#invalid_one_time_password" do
+    let(:flash) { ActionDispatch::Flash::FlashHash.new }
 
-      before(:each) do
-        controller.stub(:render)
-        controller.stub(:flash).and_return(flash)
-      end
+    before(:each) do
+      controller.stub(:render)
+      controller.stub(:flash).and_return(flash)
+    end
 
-      it 'tells the controller to render the new template' do
-        controller.should_receive(:render).with('new', status: 403)
-        listener.send(method, login_ticket)
-      end
-
-      it 'assigns a new login ticket' do
-        listener.send(method, login_ticket)
-        controller.instance_variable_get(:@login_ticket).should == login_ticket
-      end
-
-      it 'should add an error message' do
-        listener.send(method, login_ticket)
-        flash[:error].should == I18n.t("login_credential_acceptor.#{method}")
-      end
+    it 'should add an error message' do
+      listener.invalid_one_time_password
+      flash[:error].should == I18n.t('validate_otp.invalid_otp')
     end
   end
 
@@ -75,24 +69,6 @@ describe CASino::Listener::LoginCredentialAcceptor do
     it 'assigns the not allowed service' do
       listener.send(:service_not_allowed, service)
       controller.instance_variable_get(:@service).should == service
-    end
-  end
-
-  context '#two_factor_authentication_pending' do
-    let(:ticket_granting_ticket) { 'TGT-123' }
-
-    before(:each) do
-      controller.stub(:render)
-    end
-
-    it 'tells the controller to render the service_not_allowed template' do
-      controller.should_receive(:render).with('validate_otp')
-      listener.send(:two_factor_authentication_pending, ticket_granting_ticket)
-    end
-
-    it 'assigns the not allowed service' do
-      listener.send(:two_factor_authentication_pending, ticket_granting_ticket)
-      controller.instance_variable_get(:@ticket_granting_ticket).should == ticket_granting_ticket
     end
   end
 end
