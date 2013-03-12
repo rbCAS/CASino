2e30962a43e78b44e886cecc024bbb15d0997dd9
Raffael Schmid <raffael@yux.ch>
2013-03-12 21:14:07 +0100

    add coverall simplecov formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'active_support/core_ext'
 require 'simplecov'
+require 'coveralls'
 
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter '/spec'
   base_path = "#{File.dirname(__FILE__)}/../"
