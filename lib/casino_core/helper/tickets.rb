7ba21fc050f3caab81b4d0308676cb5901db5cdd
Nils Caspar <ncaspar@me.com>
2013-03-30 15:24:40 +0100

    Use SecureRandom to generate ticket strings

--- lib/casino_core/helper/tickets.rb
+++ lib/casino_core/helper/tickets.rb
@@ -1,8 +1,15 @@
+require 'securerandom'
+
 module CASinoCore
   module Helper
     module Tickets
+
+      ALLOWED_TICKET_STRING_CHARACTERS = ('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a
+
       def random_ticket_string(prefix, length = 40)
-        random_string = rand(36**length).to_s(36)
+        random_string = SecureRandom.random_bytes(length).each_char.map do |char|
+          ALLOWED_TICKET_STRING_CHARACTERS[(char.ord % ALLOWED_TICKET_STRING_CHARACTERS.length)]
+        end.join
         "#{prefix}-#{Time.now.to_i}-#{random_string}"
       end
     end
