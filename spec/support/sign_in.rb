e90d42620ff8d3a12945ddf1640cce7746d33a4f
Nils Caspar <ncaspar@me.com>
2013-04-21 16:13:04 +0200

    Remove unused test code

--- spec/support/sign_in.rb
+++ spec/support/sign_in.rb
@@ -1,15 +1,3 @@
-def test_sign_in(options = {})
-  request.env['HTTP_USER_AGENT'] = options[:user_agent] || 'TestBrowser 1.2'
-  ticket = TicketGrantingTicket.create!({
-    ticket: controller.random_ticket_string('TGC'),
-    username: options[:username] || 'user1',
-    extra_attributes: options[:extra_attributes],
-    user_agent: request.env['HTTP_USER_AGENT']
-  })
-  request.cookies[:tgt] = ticket.ticket
-  return ticket
-end
-
 def integration_sign_in(options = {})
   visit login_path
   fill_in 'username', with: options[:username] || 'testuser'
