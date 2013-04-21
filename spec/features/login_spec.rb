e29f4983a450845170e3bbe0157c4810d14c12c6
Nils Caspar <ncaspar@me.com>
2013-04-21 16:54:25 +0200

    More testing on login screen

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -8,18 +8,21 @@ describe 'Login' do
   context 'with valid username and password' do
     before { sign_in }
 
-    it { should have_link('Logout') }
+    it { should_not have_button('Login') }
+    its(:current_path) { should == sessions_path }
   end
 
   context 'with invalid username' do
     before { sign_in username: 'lalala', password: 'foobar123' }
 
     it { should have_button('Login') }
+    it { should have_text('Incorrect username or password') }
   end
 
   context 'with blank password' do
     before { sign_in password: '' }
 
     it { should have_button('Login') }
+    it { should have_text('Incorrect username or password') }
   end
 end
