dfb32e0370be4f5421500a8ab9e35876d80ae7ef
Nils Caspar <ncaspar@me.com>
2014-08-02 21:35:41 +0200

    Fix redirection for installations with custom root path, refs #58

--- app/assets/javascripts/casino/application.js
+++ app/assets/javascripts/casino/application.js
@@ -1,1 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
+(function(win) {
+  if(!win.CASino) {
+    win.CASino = { baseUrl: '/' };
+  }
+
+  win.CASino.url = function(path) {
+    return win.CASino.baseUrl + path;
+  }
+})(this);
