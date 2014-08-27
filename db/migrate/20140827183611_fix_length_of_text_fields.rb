c24c5bd3a0c37a808b8d8592ddb35288203b64d0
Nils Caspar <ncaspar@me.com>
2014-08-27 20:40:57 +0200

    Fix length of text fields

---
+++ db/migrate/20140827183611_fix_length_of_text_fields.rb
@@ -0,0 +1,7 @@
+class FixLengthOfTextFields < ActiveRecord::Migration
+  def change
+    change_column :casino_proxy_tickets, :service, :text, :limit => nil
+    change_column :casino_service_tickets, :service, :text, :limit => nil
+    change_column :casino_ticket_granting_tickets, :user_agent, :text, :limit => nil
+  end
+end
