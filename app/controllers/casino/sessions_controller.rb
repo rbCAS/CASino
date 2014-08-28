d997f051cf25c276225644a3ccffbbf41b8a5649
Nils Caspar <ncaspar@me.com>
2014-08-28 20:31:33 +0200

    Cleanup

--- app/controllers/casino/sessions_controller.rb
+++ app/controllers/casino/sessions_controller.rb
@@ -53,30 +53,6 @@ class CASino::SessionsController < CASino::ApplicationController
     end
   end
 
-  def user_logged_in(url, ticket_granting_ticket, cookie_expiry_time = nil)
-    @controller.cookies[:tgt] = { value: ticket_granting_ticket, expires: cookie_expiry_time }
-    if url.nil?
-      @controller.redirect_to sessions_path, status: :see_other
-    else
-      @controller.redirect_to url, status: :see_other
-    end
-  end
-
-  def two_factor_authentication_pending(ticket_granting_ticket)
-    assign(:ticket_granting_ticket, ticket_granting_ticket)
-    @controller.render 'validate_otp'
-  end
-
-  def invalid_login_credentials(login_ticket)
-    @controller.flash.now[:error] = I18n.t('login_credential_acceptor.invalid_login_credentials')
-    rerender_login_page(login_ticket)
-  end
-
-  def invalid_login_ticket(login_ticket)
-    @controller.flash.now[:error] = I18n.t('login_credential_acceptor.invalid_login_ticket')
-    rerender_login_page(login_ticket)
-  end
-
   def ensure_service_allowed
     if params[:service].present? && !service_allowed?(params[:service])
       render 'service_not_allowed', status: :forbidden
