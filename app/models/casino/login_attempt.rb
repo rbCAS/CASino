83f1f7c7370375abd2a7f0a9ca3a8a0ec810f604
Philipp <philipp@nine.ch>
2016-05-24 14:27:30 +0200

    fixed migration mess

--- app/models/casino/login_attempt.rb
+++ app/models/casino/login_attempt.rb
@@ -2,10 +2,4 @@ class CASino::LoginAttempt < ActiveRecord::Base
   include CASino::ModelConcern::BrowserInfo
 
   belongs_to :user
-
-  def username=(username)
-    super
-
-    self.user = CASino::User.find_by_username(username)
-  end
 end
